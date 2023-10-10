a529b20dc33d35f61245f5c2139faba7fb3f3a85
Dragos Tanta <dragos.tanta@gmail.com>
2023-10-10 21:38:19 +0300

    curs10 final

---
+++ src/curs10/TestPropertiesFile.java
@@ -0,0 +1,22 @@
+package curs10;
+
+//CRUD operations
+//Create
+//Read
+//Update
+//Delete
+
+
+public class TestPropertiesFile {
+
+	public static void main(String[] args) {
+		PropertiesFileProcessor obj =  new PropertiesFileProcessor();
+		obj.writePropertiesFile();
+		obj.readPropertiesFile("hostname");	
+		obj.readPropertiesFile("email");	
+		obj.updatePropertiesFile("hostname", "google cloud");
+		obj.readPropertiesFile("hostname");	
+
+	}
+
+}
